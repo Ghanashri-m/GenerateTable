3e6b23e899c0aff41659c08f99e0c41e9185028f
Ghanashri-m <ghanashri.28@gmail.com>
2023-06-23 17:59:59 -0700

    latest commit TS

--- in-typescript/src/App.tsx
+++ in-typescript/src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import {FormEvent, useState} from 'react';
 import './App.css';
 
-function App() {
+interface TableProps {
+  rows: number;
+  columns: number;
+}
+
+const Table: React.FC<TableProps> = ({rows, columns}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <table>
+      <tbody>
+        {Array.from({ length: rows }, () => 0).map(
+          (_, row) => (
+            <tr key={row}>
+              {
+                Array.from({ length: columns }, () => 0).map(
+                (_, col) => (
+                  <td key={col}>
+                    {col % 2 === 0
+                      ? rows * col + (row + 1)
+                      : rows * (col + 1) - row}
+                  </td>
+                ),
+              )}
+            </tr>
+          ),
+        )}
+      </tbody>
+    </table>
+  )
+}
+
+function App() {
+  const [rows, setRows] = useState<number>(0);
+  const [cols, setCols] = useState<number>(0);
+
+  return (
+    <div className='app'>
+      <form onSubmit={(e: FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          const row = formData.get('rows');
+          const col = formData.get('cols');
+          console.log(col);
+          if (typeof row === 'string' && typeof col === 'string') {
+            setRows(parseInt(row, 10));
+            setCols(parseInt(col, 10));
+          }
+        }}>
+        <div>
+          <label htmlFor='rows'>Rows</label>
+          <input type='number' name='rows' id='rows' min={1} defaultValue={rows} />
+        </div>
+        <div>
+          <label htmlFor='cols'>Columns</label>
+          <input type='number' name='cols' id='cols' min={1} defaultValue={cols} />
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+      {Boolean(rows) && Boolean(cols) && (
+      <Table rows={rows} columns={cols} />
+    )}
     </div>
   );
 }
